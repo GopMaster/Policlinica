8e6f5a9ea983cb6eeaa63102542171365ac7b55c
PC <PC@VIRGIL>
2025-03-03 19:16:12 +0200

    Add project files.

---
+++ Policlinica/Medic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Policlinica
+{
+   public  class Medic : Persoana
+    {
+
+        string Specializare;
+        int NrOperatiuni;
+
+        
+        public Medic(Persoana pers , string _Specializare) : base(pers.Nume, pers.Prenume, pers.age, pers.Gen, pers.nrTelefon, pers.email)
+        {
+            Specializare = _Specializare;
+            int nrOperatiunil = 0;
+        
+        }
+
+
+    }
+}
---
+++ Policlinica/Persoana.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Policlinica
+{
+    public class Persoana
+    {
+
+        public string Nume;
+        public string Prenume;  
+        public int age;
+        public char Gen;
+        public string nrTelefon;
+        public string email;
+
+        public Persoana() {
+            Nume = string.Empty;
+            Prenume = string.Empty;
+            age = 0;
+            Gen = '?';
+            nrTelefon = string.Empty;
+        }
+        public Persoana(string _nume , string _prenume, int _age, char _gen, string _nrTelefon , string _email)
+        {
+            Nume = _nume;
+            Prenume =_prenume;
+            this.age =_age;
+            Gen = _gen;
+            this.nrTelefon = _nrTelefon;
+            email = _email;
+        }
+
+
+
+
+    }
+}
